e417d587f6111df3f4a99aa92afd433fdf067a5f
Natsuki <pxde9ise@s.okayama-u.ac.jp>
2026-01-29 17:32:46 +0900

    Initial commit

---
+++ score.py
@@ -0,0 +1,30 @@
+import pygame
+
+
+class ScoreCounter:
+    def __init__(self,screen,x,y,font_size):
+        self.screen = screen
+
+        #スコアを保存する変数
+        self.score = 0
+
+        self.x = x
+        self.y = y
+        self.size = font_size
+
+        #フォントを指定
+        self.font = pygame.font.SysFont("MS Gothic", font_size)
+
+        self.surface = self.font.render(f"SCORE:{self.score}", True, (255,255,255))
+        self.rect = self.surface.get_rect(topleft=(self.x, self.y))
+
+        self.count = 0
+
+    def update(self):
+        self.count = self.count + 1
+        if self.count == FPS:
+            self.score += 10
+            self.count = 0
+
+        self.surface = self.font.render(f"SCORE:{self.score}", True, (255,255,255))
+        self.screen.blit(self.surface, self.rect)
